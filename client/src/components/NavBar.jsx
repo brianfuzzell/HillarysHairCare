dac37fedbec8e76a8615ff6667d8951e12052557
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-06-12 09:13:43 -0500

    Reformatted to a function expression

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
-export default function NavBar() {
+export const NavBar = () => {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
